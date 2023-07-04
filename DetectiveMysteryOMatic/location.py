d469746dbf72464228c940ce8b452142f393005c
neuromancer <gustavo.grieco@gmail.com>
2023-07-04 22:47:56 +0200

    graph rendering fixes

--- DetectiveMysteryOMatic/location.py
+++ DetectiveMysteryOMatic/location.py
@@ -4,7 +4,7 @@ from networkx.drawing.nx_agraph import to_agraph
 
 mansion_locations = {0: "KITCHEN", 1: "LIVING", 2: "BEDROOM", 3: "BATHROOM"}
 weapon_locations = {"KITCHEN": "gun", "LIVING": "knife", "BEDROOM": "poison", "BATHROOM": "rope"}
-mansion_representations = {0: "🍲", 1: "🛋️", 2: "🛏️", 3: "🚽"}
+mansion_representations = {"KITCHEN": "kitchen 🍲", "LIVING": "living room 🛋️", "BEDROOM": "bedroom 🛏️", "BATHROOM": "bathroom 🚽"}
 
 def create_locations_graph(outdir, nodes):
 	graph = gnr_graph(4, 0.5).to_undirected()
@@ -12,6 +12,7 @@ def create_locations_graph(outdir, nodes):
 	return graph
 
 def render_locations(outdir, graph):
+	graph = relabel_nodes(graph, mansion_representations)
 	g = to_agraph(graph)
-	g.node_attr.update(color = 'lightblue2', style = 'filled')
+	g.node_attr.update(color = "lightblue2", style = "filled", shape = "Mrecord")
 	g.draw(outdir + "/images/locations.svg", prog='dot')
