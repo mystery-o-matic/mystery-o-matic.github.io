290f7b995a4a693faad42a61151ba6976561e2dc
neuromancer <gustavo.grieco@gmail.com>
2023-10-15 08:38:58 +0200

    correctly handle multile users in telegram

--- mystery_o_matic/output/text/telegram.py
+++ mystery_o_matic/output/text/telegram.py
@@ -29,7 +29,13 @@ async def command(shell, update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     if update.message.text is None:
         return
     command = update.message.text.replace("/", "").lower()
+    chat_id = update.message.chat.id
+    print("Reciving message from", update.message.from_user)
+    current_clue = shell.current_clue_per_chat.get(chat_id, 0)
+    shell.current_clue = current_clue
     shell.onecmd(command)
+    shell.current_clue_per_chat[chat_id] = shell.current_clue
+
     last_output = shell.stdout.last_output
     shell.stdout.reset()
     if last_output == "":
@@ -39,6 +45,7 @@ async def command(shell, update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
 
 def create_telegram_bot(shell, api_key):
     shell.stdout = BufferStdout()
+    shell.current_clue_per_chat = dict()
     app = ApplicationBuilder().token(api_key).build()
     app.add_handler(MessageHandler(filters.ALL, (lambda u, c: command(shell, u, c))))
     return app
