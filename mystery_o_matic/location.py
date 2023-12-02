dd32c57f9790d8de29a339eff956133bf66773aa
neuromancer <gustavo.grieco@gmail.com>
2023-12-02 15:24:46 +0100

    fix

--- mystery_o_matic/location.py
+++ mystery_o_matic/location.py
@@ -45,7 +45,7 @@ def render_locations(outdir, graph):
     g = to_agraph(relabeled_graph)
     g.graph_attr.update(bgcolor="transparent")
     g.node_attr.update(
-        fontname="Raleway", color="lightblue2", style="filled", shape="Mrecord"
+        fontname="Raleway", color="lightblue2", style="filled",  shape="Mrecord"
     )
     g.edge_attr.update(color="gray")
     g.draw(outdir + "/images/locations_big.svg", prog="dot")
