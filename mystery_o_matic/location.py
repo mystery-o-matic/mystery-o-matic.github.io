0610cb45f0352b66e226034adfa30690566dc525
neuromancer <gustavo.grieco@gmail.com>
2023-12-02 14:32:44 +0100

    mini-map fixes

--- mystery_o_matic/location.py
+++ mystery_o_matic/location.py
@@ -47,6 +47,7 @@ def render_locations(outdir, graph):
     g.node_attr.update(
         fontname="Raleway", color="lightblue2", style="filled", shape="Mrecord"
     )
+    g.edge_attr.update(color="gray")
     g.draw(outdir + "/images/locations_big.svg", prog="dot")
     g.graph_attr.update(dpi="200")
     g.draw(outdir + "/images/locations_big.png", prog="dot")
@@ -57,7 +58,9 @@ def render_locations(outdir, graph):
 
     relabeled_graph = relabel_nodes(graph, labels)
     g = to_agraph(relabeled_graph)
-    g.graph_attr.update(bgcolor="transparent", nodesep="0.1")
+    g.graph_attr.update(bgcolor="transparent", nodesep="0.1", ranksep="0.1")
+    g.edge_attr.update(color="gray", labeldistance="0.1")
+
     g.node_attr.update(
         fontname="Raleway", shape="plaintext"
     )
