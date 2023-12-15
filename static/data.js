973b523fbc71f7347d01b055bd516fe4a6a848a3
neuromancer <gustavo.grieco@gmail.com>
2023-12-15 08:54:41 +0100

    improved tutorial and example data

--- static/data.js
+++ static/data.js
@@ -1,13 +1,47 @@
-// This data is only for previewing the template file
 data = {
-	numIntervals: 10,
-	nColumns: 8,
-	timeOffset: 32400,
-	suspectNames: ["alice", "bob", "carol"],
-	victim: "alice",
-	finalLocationsMap: {
-		"bob": "kitchen",
-		"carol": "bedroom",
-		"alice": "kitchen"
-	},
+    "numIntervals": 10,
+    "suspectNames": [
+        "eddie",
+        "dave",
+        "bob"
+    ],
+    "victim": "dave",
+    "finalLocationsMap": {
+        "eddie": "kitchen",
+        "dave": "bedroom",
+        "bob": "kitchen"
+    },
+    "representationsMap": {
+        "kitchen": "🍲",
+        "dining room": "🍽️",
+        "bedroom": "🛏️",
+        "bathroom": "🚽"
+    },
+    "weaponsMap": {
+        "knife": "kitchen",
+        "poison": "dining room",
+        "pistol": "bedroom",
+        "rope": "bathroom"
+    },
+    "timeOffset": 32400,
+    "additionalClues": [
+        "<a href=\"#/\" class=\"link-dark\" onClick=\"openModal('eddie')\">Eddie</a> said: \"I think <a href=\"#/\" class=\"link-dark\" onClick=\"openModal('bob')\">Bob</a> was not in the bedroom at 10:30\"",
+        "<a href=\"#/\" class=\"link-dark\" onClick=\"openModal('eddie')\">Eddie</a> said: \"I think <a href=\"#/\" class=\"link-dark\" onClick=\"openModal('dave')\">Dave</a> was not in the kitchen at 10:15\"",
+        "<a href=\"#/\" class=\"link-dark\" onClick=\"openModal('eddie')\">Eddie</a> said: \"I was in the bedroom from 10:30 to 10:45\"",
+        "<a href=\"#/\" class=\"link-dark\" onClick=\"openModal('bob')\">Bob</a> said \"I saw nobody when I was leaving the dining room at 10:00\"",
+        "Inspecting the body reveals that the poison was not the murderer weapon",
+        "When inspecting the body, it becomes clear that the pistol was not the weapon used by the murderer.",
+        "A close examination of the body reveals that the murder took place either at 10:15 or at 10:30",
+        "<a href=\"#/\" class=\"link-dark\" onClick=\"openModal('eddie')\">Eddie</a> said: \"I'm sure <a href=\"#/\" class=\"link-dark\" onClick=\"openModal('dave')\">Dave</a> was not in the kitchen at 11:00\"",
+        "Inspecting the body reveals that the rope was not the murderer weapon",
+        "The pathologist says that murder took place either at 10:30 or at 10:45",
+        "<a href=\"#/\" class=\"link-dark\" onClick=\"openModal('eddie')\">Eddie</a> said \"I'm sure I saw somebody when I arrived to the kitchen at 11:00\"",
+        "<a href=\"#/\" class=\"link-dark\" onClick=\"openModal('bob')\">Bob</a> said: \"I was in the dining room from 9:00 to 10:00\"",
+        "<a href=\"#/\" class=\"link-dark\" onClick=\"openModal('eddie')\">Eddie</a> said: \"<a href=\"#/\" class=\"link-dark\" onClick=\"openModal('bob')\">Bob</a> was not in the bedroom at 10:45\"",
+        "\"I was in the kitchen from 9:00 to 10:15\" stated <a href=\"#/\" class=\"link-dark\" onClick=\"openModal('eddie')\">Eddie</a>",
+        "<a href=\"#/\" class=\"link-dark\" onClick=\"openModal('bob')\">Bob</a> said: \"I'm sure <a href=\"#/\" class=\"link-dark\" onClick=\"openModal('dave')\">Dave</a> was not in the kitchen at 10:15\"",
+        "<a href=\"#/\" class=\"link-dark\" onClick=\"openModal('bob')\">Bob</a> said \"I saw somebody when I arrived to the kitchen at 10:15\"",
+        "<a href=\"#/\" class=\"link-dark\" onClick=\"openModal('eddie')\">Eddie</a> said \"I saw <a href=\"#/\" class=\"link-dark\" onClick=\"openModal('bob')\">Bob</a> when I was leaving the kitchen at 10:15\""
+    ],
+    "correctAnswer": "ef1a3dcb738fc6dfe7bb98fc1de7760b92eafd538863f0c2daaa5f4405049776"
 }
