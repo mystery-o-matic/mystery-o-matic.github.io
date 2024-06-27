3cd5a7017cac0609632895a460651e5b074af49e
neuromancer <gustavo.grieco@gmail.com>
2024-06-27 08:24:24 +0200

    more fixes

--- static/translation.js
+++ static/translation.js
@@ -1,13 +1,3 @@
-/*function googleTranslateElementInit() {
-	new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
-}*/
-
-// Create a new onchange event and trigger it
-const triggerEvent = (element,eventName) =>{
-	const event = new Event(eventName, {'bubbles': true});
-	element.dispatchEvent(event);
-};
-
 function checkIfWebsiteShouldBeTranslated(force) {
 	language = window.navigator.language;
 	language = language.split("-")[0];
@@ -18,15 +8,8 @@ function checkIfWebsiteShouldBeTranslated(force) {
 	}
 }
 
-function keepInEnglish() {
-	sessionStorage.setItem("language", "");
-	//var goog_te_combo = document.getElementsByClassName("goog-te-combo")[0];
-	//goog_te_combo.value = "en";
-	//triggerEvent(document.querySelector('.goog-te-combo'), 'change');
-}
-
 function translateContent(language) {
-	sessionStorage.setItem("language", language);
+	window.location.replace("../" + language);
 }
 
 checkIfWebsiteShouldBeTranslated(false);
